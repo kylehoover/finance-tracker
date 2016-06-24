5034e36d4a8f084d85b4c52e1f40b0b50e5bc09c
Kyle Hoover <khoov22@gmail.com>
2016-06-24 16:52:28 -0600

    Add instance variables to Transaction

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.*;
+import src.app.Transaction;
 
 public class Main
 {
---
+++ src/app/Transaction.java
@@ -0,0 +1,32 @@
+package src.app;
+
+public class Transaction
+{
+  // static variables
+  private static int currentID = 0;
+
+  // instance variables
+  private int id;
+
+  private double amount;
+
+  private String description;
+  private String location;
+  private String vendor;
+
+  // enums
+  // account type: savings, checking, credit
+  // transaction type: withdrawal, deposit ... credit => advance, payment (probably exclude these)
+
+  public Transaction()
+  {
+    id = currentID++;
+  }
+
+  // getter methods
+  public int getID()
+  {
+    return id;
+  }
+
+}
