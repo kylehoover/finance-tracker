33f1b3d9aa40556fce60edfc71b4bf41ff32030c
Kyle Hoover <khoov22@gmail.com>
2016-06-25 12:21:50 -0600

    Add Transaction.Builder for creating new Transaction objects

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 package src;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+
 import src.app.Transaction;
 
 public class Main
 {
   public static void main(String[] args)
   {
-    Transaction t = new Transaction();
-    Transaction s = new Transaction();
-    Transaction e = new Transaction();
+    Transaction t = new Transaction.Builder("Smiths", 34.83, "6/25/2016").build();
+    Transaction t2 = new Transaction.Builder("Swigs", 2.00, "11/3/2013").description("sugar cookie").location("Provo").build();
 
-    System.out.println(t.getID());
-    System.out.println(s.getID());
-    System.out.println(e.getID());
+    System.out.println(t.toString());
+    System.out.println(t2.toString());
+
   }
 }
--- src/app/Transaction.java
+++ src/app/Transaction.java
@@ -1,26 +1,38 @@
 package src.app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Transaction
 {
   // static variables
   private static int currentID = 0;
 
   // instance variables
-  private int id;
+  private final int id;
 
-  private double amount;
+  private final double amount;
 
-  private String description;
-  private String location;
-  private String vendor;
+  private final Date date;
+  private final String description;
+  private final String location;
+  private final String vendor;
+
+  // Category
 
   // enums
   // account type: savings, checking, credit
   // transaction type: withdrawal, deposit ... credit => advance, payment (probably exclude these)
 
-  public Transaction()
+  private Transaction(Builder builder)
   {
     id = currentID++;
+    amount = builder.amount;
+    date = builder.date;
+    description = builder.description;
+    location = builder.location;
+    vendor = builder.vendor;
   }
 
   // getter methods
@@ -29,4 +41,54 @@ public class Transaction
     return id;
   }
 
+  public String toString()
+  {
+    SimpleDateFormat sdf = new SimpleDateFormat("M/d/y");
+    return getClass().getName() + "[id=" + id + ",amount=" + amount + ",date=" + sdf.format(date) + ",description=" + description + ",location=" + location + ",vendor=" + vendor + "]";
+  }
+
+  public static class Builder
+  {
+    // required parameters
+    private final double amount;
+    private final Date date;
+    private final String vendor;
+
+    // optional parameters
+    private String description = "";
+    private String location = "";
+
+    public Builder(String vendor, double amount, String date)
+    {
+      this.amount = amount;
+      this.vendor = vendor;
+
+      SimpleDateFormat sdf = new SimpleDateFormat("M/d/y");
+      Date d;
+      try {
+        d = sdf.parse(date);
+      } catch (ParseException e) {
+        d = null;
+      }
+
+      this.date = d;
+    }
+
+    public Builder description(String description)
+    {
+      this.description = description;
+      return this;
+    }
+
+    public Builder location(String location)
+    {
+      this.location = location;
+      return this;
+    }
+
+    public Transaction build()
+    {
+      return new Transaction(this);
+    }
+  }
 }
